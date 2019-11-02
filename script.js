fcf3f8c41c60b131709d105a7a859ecf064fc728
Malcolm Childs <michilds0@gmail.com>
2019-11-02 18:50:03 -0400

    Minor updates

--- script.js
+++ script.js
@@ -1,65 +1,71 @@
-const app = (function() {
+const app = (function () {
+
+    const log = (value, text = "[LOG]: ") => {
+        console.log(text, value);
+        return value;
+    };
 
     let activeSection;
     const body = document.querySelector('body');
     const nav = document.querySelector('.js-nav');
+    const navBorder = document.querySelector('.js-nav-item--active__border');
     const navButton = document.querySelector('.js-nav-button');
-    const previousOffset = []; 
-    let timeoutId;   
+    const navItems = document.querySelectorAll('.js-nav-item');
+    const navItemsArray = Array.from(navItems);
+    const sections = document.querySelectorAll('.js-section');
 
-    const animateNavItemBorder = (target) => {
-        const navBorder = document.querySelector('.js-nav-item--active__border');
-        previousOffset[0] = target.offsetLeft;
-        const currentOffset = target.offseLeft >= previousOffset[0] ? -target.offsetLeft : target.offsetLeft;
-        navBorder.style.transform = `translateX(${currentOffset}px)`;
+    const findCssClass = (element, className) => element.classList.contains(className);
+
+    const getActiveNavItem = (element) => findCssClass(element, "nav-item--active");
+
+    const animateNavItemBorder = () => {
+        const activeItem = navItemsArray.filter(getActiveNavItem)[0];
+        const activeItemIndex = navItemsArray.indexOf(activeItem);
+        navBorder.style.transform = `translateX(${(activeItemIndex * 100)}%)`;
     }
 
     const setActiveNavItem = (target) => {
-        const navItems = document.querySelectorAll('.js-nav-item');
         navItems.forEach((navItem) => navItem.classList.remove('nav-item--active'));
         target.classList.add('nav-item--active');
     }
 
     const setActiveClass = (target) => {
-        const sections = document.querySelectorAll('.js-section');
         sections.forEach((section) => section.classList.remove('section--active'));
         target.classList.add('section--active');
     }
 
     const setActiveSectionElement = (target) => {
         const navItemText = target.getElementsByTagName('p')[0];
-        activeSection = document.getElementById(navItemText.dataset.id);
+        return document.getElementById(navItemText.dataset.id);
     }
-    
+
     const scrollToSection = (sectionElement) => {
         sectionElement.scrollIntoView({
-            behavior: 'smooth', 
+            behavior: 'smooth',
             inline: 'center'
         });
     }
 
     const toggleNavMenu = () => {
-        if (body.classList.contains('nav--visible')) {
-            body.classList.add('nav--hidden');
-            body.classList.remove('nav--visible');
-        } else {
-            body.classList.add('nav--visible');
-            body.classList.remove('nav--hidden');
-        }
+        // nav--hidden already exists on the body so it will be removed
+        body.classList.toggle('nav--hidden');
+
+        // nav--visible doesn't exist on the body so it will be added
+        body.classList.toggle('nav--visible');
     }
 
-    const handleNavItemClick = ({target}) => {
-        const currentEl = target.classList.contains('.js-nav-item') ? target : target.parentNode;
-        setActiveSectionElement(currentEl);
+    const handleNavItemClick = ({ target }) => {
+        const currentEl = findCssClass(target, '.js-nav-item') ? target : target.parentNode;
         setActiveNavItem(currentEl);
-        animateNavItemBorder(currentEl);
+        animateNavItemBorder();
+        activeSection = setActiveSectionElement(currentEl);
         scrollToSection(activeSection);
         setActiveClass(activeSection);
     }
 
     const run = () => {
-        navButton.addEventListener('click', toggleNavMenu);
         nav.addEventListener('click', handleNavItemClick);
+        navButton.addEventListener('click', toggleNavMenu);
     }
 
     return { run }
